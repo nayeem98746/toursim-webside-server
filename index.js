1f80baee0fafa017f6b2b1d8724c2ea0e03c10e5
nayeem407 <sabbirahmednayeem941@gmail.com>
2021-11-01 19:40:44 +0600

    added port

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ async function run(){
         const database = client.db('tourismWebside');
         const servicesCollection = database.collection('services')
         const imagesCollection = database.collection('images')
+        const vehicleCollection = database.collection('vehicle')
+        const userCollection = database.collection('users')
         
 
         // get single service
@@ -59,27 +61,87 @@ async function run(){
             console.log(result);
             res.json(result)
 
-
-
-
-            //img post
-            app.post('/images' , async(req, res)=> {
-              console.log('hit the post image') 
-                // const result = await imagesCollection.insertOne(image)
-                // console.log(result)
-                res.send('post hitet')
-
-            })
-
-
-        // get img
-        app.get('/images', async(req, res)=> {
-            const cursor = imagesCollection.find({})
-            const images = await cursor.toArray()
-            res.send(images)
-        })
-
         });
+         //img post
+         app.post('/images' , async(req, res)=> {
+            const images = req.body;
+            console.log('hit the post api',images)
+            const result = await imagesCollection.insertOne(images)
+            console.log(result)
+            res.json('post hitted')
+
+        })
+        // get single img id
+        app.get('/images/:id', async(req, res)=> {
+            const id = req.params.id
+            console.log('geitting specific images', id)
+            const query = {_id : ObjectId(id)}
+            const images = await imagesCollection.findOne(query)
+            res.json(images)
+        })
+    // get img
+    app.get('/images', async(req, res)=> {
+        const cursor = imagesCollection.find({})
+        const images = await cursor.toArray()
+        res.send(images)
+    })
+
+    //get vehicles
+    app.post('/vehicles', async(req, res)=>{
+        const vehicles = req.body;
+        console.log('hit the vehicles post api', vehicles)
+        const result = await vehicleCollection.insertOne(vehicles)
+        console.log(result)
+        res.send('post hitted')
+    })
+    //get slected vehicles
+    app.get('/vehicles/:id', async(req, res)=> {
+        const id = req.params.id
+        console.log('getting specific vehicls', id)
+        const query = { _id : ObjectId(id)}
+        const vehisles = await vehicleCollection.findOne(query)
+        res.json(vehisles)
+    })
+
+
+
+    // get vehicles
+    app.get('/vehicles', async(req, res)=> {
+        const cursor = vehicleCollection.find({})
+        const vehicles = await cursor.toArray()
+        res.send(vehicles)
+    })
+
+    // get api
+    app.get('/users' , async(req, res )=> {
+        const cursor = userCollection.find({})
+        const users = await cursor.toArray()
+        res.send(users)
+    })
+    // users post api
+    app.post('/users', async(req , res)=> {
+        const newUser = req.body
+        const result = await userCollection.insertOne(newUser)
+       
+        res.json(result)
+    })
+
+    //delete api
+    app.delete('/users/:id' , async(req, res)=> {
+        const id = req.params.id
+        const query ={_id:ObjectId(id)}
+        const result = await userCollection.deleteOne(query)
+        res.json(result)
+    })
+    // update api
+    app.get('/user/:id', async(req, res)=> {
+        const id = req.params.id
+        const query = {_id: ObjectId(id)}
+        const user = await userCollection.find(query)
+        console.log('load user with id : ' , id)
+        res.send(user)
+    })
+    
     }
     finally{
         // await client.close()
